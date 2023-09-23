86e41d00538b9e1e83c6103ba8b4584c88f925ba
shellwe <benjaminshellwe@outlook.com>
2023-09-23 22:27:25 +0800

    back ver 0.0.1.2

--- src/main/java/top/shellwe/toolbox/controller/solution.java
+++ src/main/java/top/shellwe/toolbox/controller/solution.java
@@ -24,7 +24,10 @@ public class solution {
         System.out.println(arrayAsString);
         return left; // 返回新长度，每次找到相等的值时被跳过不会自增
     }
-    /***/
+    /*
+     * 给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。如果目标值不存在于数组中，返回它将会被按顺序插入的位置。
+        请必须使用时间复杂度为 O(log n) 的算法。
+     */
     public static int searchInsert(int[] nums, int target) {
         for(int right = 0; right < nums.length; right++){
             if(nums[right] >= target){
